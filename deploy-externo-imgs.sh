cb51e3a4836f76e83558cb9d0929654b702c77f0
Matheus Morselli Gysi <esc.matheus@gmail.com>
2024-10-31 13:48:16 -0300

    Update deploy-externo-imgs.sh

--- deploy-externo-imgs.sh
+++ deploy-externo-imgs.sh
@@ -7,22 +7,22 @@ linha_secao_nao_essencial=`grep -n "$string_secao_nao_essencial" env_files/secur
 
 if [ `head -$linha_secao_nao_essencial env_files/security.env|grep --count '\*\*\*\*'` -gt 0 ]; then
   echo "==========================================="
-  echo "ATENÇÃO: Deploy do SEI IA foi interrompido!"
+  echo "ATENÇÃO: Deploy Servidor de Soluções do SEI-IA foi interrompido!"
   echo "==========================================="
   echo "A seção de configurações ESSENCIAIS do arquivo env_files/security.env não está adequadamente preenchida!"
-  echo "O deploy do SEI IA depende da configuração de todas as variáveis dessa seção do arquivo!"
-  echo "(para mais detalhes sobre o correto preenchimento do arquivo env_files/security.env, leia o arquivo README.md)"
+  echo "O deploy do Servidor de Soluções do SEI-IA depende da configuração de todas as variáveis dessa seção do arquivo!"
+  echo "(para mais detalhes sobre o correto preenchimento do arquivo env_files/security.env, leia o arquivo docs/INSTALL.md)"
   exit 1
 fi
 
 if [ `tail -n +$linha_secao_nao_essencial env_files/security.env|grep --count '\*\*\*\*'` -gt 0 ]; then
   echo "==============================================="
-  echo "ATENÇÃO: o deploy do SEI IA pode ter problemas!"
+  echo "ATENÇÃO: o deploy do Servidor de Soluções do SEI-IA pode ter problemas!"
   echo "==============================================="
   echo "A seção de configurações NÃO ESSENCIAIS do arquivo env_files/security.env não está totalmente preenchida!"
   echo "Embora nem todas as varíaveis dessa seção sejam de preenchimento obrigatório, é importante notar que" \
-       "o deploy do SEI IA pode dar errado dependendo do que não foi configurado nessa seção do arquivo!"
-  echo "(para mais detalhes sobre o correto preenchimento do arquivo env_files/security.env, leia o arquivo README.md)"
+       "o deploy do Servidor de Soluções do SEI-IA pode dar errado dependendo do que não foi configurado nessa seção do arquivo!"
+  echo "(para mais detalhes sobre o correto preenchimento do arquivo env_files/security.env, leia o arquivo docs/INSTALL.md)"
 fi
 
 echo "*** `date`: Carregando variáveis de ambiente..."
@@ -36,10 +36,10 @@ cat env_files/prod.env > .env
 cat env_files/default.env >> .env
 cat env_files/security.env >> .env
 
-echo "*** `date`: Criando pasta de storage para o SEI IA caso não exista..."
+echo "*** `date`: Criando pasta de storage para o SEI-IA caso não exista..."
 [ -d $STORAGE_PROJ_DIR ] && chmod 777 $STORAGE_PROJ_DIR || mkdir --mode 777 $STORAGE_PROJ_DIR
 
-echo "*** `date`: Configurando variáveis de ambiente para instalação do SEI IA..."
+echo "*** `date`: Configurando variáveis de ambiente para instalação do Servidor de Soluções do SEI-IA..."
 export PROJECT_NAME=sei_ia
 
 export API_SEI_IMAGE="0.3-RC"
@@ -52,15 +52,17 @@ export POSTGRES_IMAGE="0.3-RC"
 
 export DOCKER_REGISTRY="anatelgovbr/"
 
-echo "*** `date`: Deploy do SEI IA em andamento..."
+echo "*** `date`: Deploy do Servidor de Soluções do SEI-IA em andamento..."
 docker compose --profile externo \
   -f docker-compose-prod.yaml \
   -p $PROJECT_NAME \
   up \
   --no-build -d
 
-echo "*** `date`:Ativando as DAGs do SEI IA no Airflow..."
+echo "*** `date`:Ativando as DAGs do Servidor de Soluções do SEI-IA no Airflow..."
 docker compose -f docker-compose-prod.yaml -p $PROJECT_NAME exec airflow-webserver-pd /bin/bash -c "airflow dags list | awk '{print \$1}' | grep -v 'DAG_ID' | xargs -I {} airflow dags unpause {}; exit 0"
 
 #adicionar registro na tabela version_register no banco sei_similaridade
 sh insert_row_version_register.sh
+
+echo "*** `date`:Finalizado o Deploy do Servidor de Soluções do SEI-IA. "
